7b1117bdf52cff904724e6fd9378d60486696369
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-06-29 23:35:50 +0200

    Add built bundle

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -2,15 +2,23 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: false,
   entry: [
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
+  ],
   module: {
     loaders: [{
       test: /\.js$/,
